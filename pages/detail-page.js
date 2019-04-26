d0db81b90fe17566e522364fc109c25103d70396
kerenKi <kkinberg13@gmail.com>
2019-04-26 16:18:01 +0200

    creating new elements an appending them

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,5 +1,23 @@
-//const userNameInput = document.getElementById('name').value;
-//console.log(userNameInput);
-function submitComment(params) {
-    console.log('hello')
+function submitComment() {
+    //getting the name and massage 
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const textareaField = document.getElementById('msg');
+    const msg = textareaField.value;
+
+    //creating a new comment 
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    //combine data with what we created
+    h3.innerText = '${name} said:'
+    p.innerText = msg;
+    comment.classList.add("comment");
+    
+    //appednding
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment)
+    
 }
