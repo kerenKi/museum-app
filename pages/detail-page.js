e5b19052d2e3d9dc53cfed73ac2d10df26cd0a19
kerenKi <kkinberg13@gmail.com>
2019-04-28 15:34:41 +0200

    form validation and add coment option

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,15 +1,36 @@
+function doesNotPassAllValidations(name,msg) {
+    if (!name || !msg){
+        alert('You forgot to fill in your name or message!')
+        return true
+    } else if (msg.length > 28){
+        alert("this is way to long! try less words")
+        return true
+    } 
+    return false
+    
+    
+    
+}
 function submitComment() {
+    
     //getting the name and massage 
     const inputField = document.getElementById('name');
     const name = inputField.value;
+    
     const textareaField = document.getElementById('msg');
     const msg = textareaField.value;
+
+    //validatation
+    if (doesNotPassAllValidations(name,msg)) {
+        console.log('no name or massage')
+        return null
+    }
 
     //creating a new comment 
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
-
+    
     //combine data with what we created
     h3.innerText = `${name} said:`
     p.innerText = msg;
@@ -18,14 +39,12 @@ function submitComment() {
     //appednding
     comment.appendChild(h3);
     comment.appendChild(p);
-
+    
     //posting the comments on the page
     const commentSection = document.getElementById('comments')
     commentSection.appendChild(comment);
-
+    
     //clearing the fields
     inputField.value = null
-    textareaField.value = null
-    console.log(msg)
-    
+    textareaField.value = null  
 }
